cbc87973dbec3eecf25060e4a18f75fc11157c01
Rahul Lal <Rahul21Lal@gmail.com>
2024-09-10 11:53:24 +1200

    Update JS
    - Working on main timer
    - Use whole numbers, no real time
    - source to num formatiing in commenting

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// setInterval(livetime, 1000);
+let minute, second;
 
+let pTime = 25;
+let sbTIme = 5;
+let lbTime = 15
+
+function btnStart_Click() {
+    console.log("Start");
+    timerActivate("hOneTimer");
+}
+
+function timerActivate (timer) {
+        // Credit: https://codepen.io/rxsharp/pen/jPZgpX
+    var element, endTime, hours, secs, msLeft, time;
+    element = document.getElementById("hOneTimer");
+
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+}
+
+function btnPause_Click() {
+    console.log("Pause");
+}
+
+function btnBreak_Click() {
+    console.log("Break");
+}
