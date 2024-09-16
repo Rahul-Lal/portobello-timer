1d89b41a0092cfc16e5149ccf8502965c5edc7e7
Rahul Lal <Rahul21Lal@gmail.com>
2024-09-17 11:57:31 +1200

    JS completed
    CSS is in progress!

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
-// setInterval(livetime, 1000);
-let minute, second;
+//Credit to source: https://jsfiddle.net/u7ahcdgn/2/
+let btnStart = document.getElementById('btnStart');
+let btnStop = document.getElementById('btnStop');
+let btnShortBreak = document.getElementById('btnShortBreak');
 
-let pTime = 25;
-let sbTIme = 5;
-let lbTime = 15
 
-function btnStart_Click() {
-    console.log("Start");
-    timerActivate("hOneTimer");
-}
+function startTimer(duration, display) {
+    let timer = duration,
+      minutes, seconds;
+    let intervalId = setInterval(function() {
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
+  
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      display.textContent = minutes + ":" + seconds;
+  
+      if (--timer < 0) {
+        timer = duration;
+      }
+      if(timer == 0 ){
+        alert("Times Up");
+        console.log("Times Up");
+      }
+    }, 1000);
+  
+    btnStop.addEventListener('click', function() {
+      clearInterval(intervalId);
+      console.log("Timer Stopped");
+      alert("Timer Stopped");
+      btnStart.disabled = false;
+      btnStop.disabled = true;
+      btnShortBreak.disabled = false;
+    });
+  }
+  
+  window.onload = function() {
+    display = document.querySelector('#time');
+    btnStart.addEventListener('click', function() {
+      startTimer(1500, display);
+      console.log("Start Portobello");
+      alert("Start Portobello");
+      btnStart.disabled = true;
+      btnStop.disabled = false;
+      btnShortBreak.disabled = true;
 
-function timerActivate (timer) {
-        // Credit: https://codepen.io/rxsharp/pen/jPZgpX
-    var element, endTime, hours, secs, msLeft, time;
-    element = document.getElementById("hOneTimer");
-
-    function twoDigits( n )
-    {
-        return (n <= 9 ? "0" + n : n);
-    }
-}
-
-function btnPause_Click() {
-    console.log("Pause");
-}
-
-function btnBreak_Click() {
-    console.log("Break");
-}
+    });
+  btnShortBreak.addEventListener('click', function() {
+    console.log("Short Break Start");
+    alert("Short Break Start");
+      startTimer(300, display);
+      
+      btnStart.disabled = true;
+      btnStop.disabled = false;
+      btnShortBreak.disabled = true;
+    });
+  
+  };
+  
